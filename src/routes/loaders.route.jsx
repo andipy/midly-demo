18d94ed5f4f5cd732a5569a67c2535de2bf3820a
andipy <bydanielromano@gmail.com>
2024-07-09 18:50:09 +0200

    refined loading page buttons

--- src/routes/loaders.route.jsx
+++ src/routes/loaders.route.jsx
@@ -6,9 +6,13 @@ import SimpleSpinnerLoader from '../components/simple-spinner-loader.component'
 const LoadersRoute = () => {
     return (
         <ContainerDefault>
-            <Button style={'d-flex-row align-items-center j-c-center gap-0_5em bg-acid-lime fsize-xs-3 f-w-600 dark-900 letter-spacing-1 pt-xs-3 pb-xs-3'} label={'Sto entrando...'}>
+            <h3 className='mt-xs-10 fsize-xs-4'>Here a loading button for visual comparison</h3>
+            <Button style={'d-flex-row align-items-center j-c-center gap-0_5em bg-acid-lime fsize-xs-3 f-w-600 dark-900 letter-spacing-1'} label={'Sto entrando...'}>
                 <SimpleSpinnerLoader place={'loader-button'} />
             </Button>
+
+            <h3 className='mt-xs-10 fsize-xs-4'>Here a non-loading button for visual comparison</h3>
+            <Button style='bg-acid-lime fsize-xs-3 f-w-600 dark-900 letter-spacing-1' label='Next' />
         </ContainerDefault>
 
     )
