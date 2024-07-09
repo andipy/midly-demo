973ef9e0b5ae2953dedf6fb8e56803957e81a352
andipy <bydanielromano@gmail.com>
2024-07-09 15:01:25 +0200

    removed useless button in loaders page

--- src/routes/loaders.route.jsx
+++ src/routes/loaders.route.jsx
@@ -9,7 +9,6 @@ const LoadersRoute = () => {
             <Button style={'d-flex-row align-items-center j-c-center gap-0_5em bg-acid-lime fsize-xs-3 f-w-600 dark-900 letter-spacing-1 pt-xs-3 pb-xs-3'} label={'Sto entrando...'}>
                 <SimpleSpinnerLoader place={'loader-button'} />
             </Button>
-            <Button style={'bg-acid-lime fsize-xs-3 f-w-600 dark-900 letter-spacing-1'} label={'Sto entrando...'} />
         </ContainerDefault>
 
     )
