434f55929f30bc9088a12c91a42eaab4dc2c2d94
andipy <bydanielromano@gmail.com>
2024-09-19 11:09:17 +0200

    button closed FL restyled

--- src/components/message-flash-leaderboard.component.jsx
+++ src/components/message-flash-leaderboard.component.jsx
@@ -46,13 +46,13 @@ const MessageFlashLeaderboard = ({ artist }) => {
                 <Button style={'button-flash-leaderboard-live d-flex-row align-items-center j-c-center bg-acid-lime border-radius-100 black w-auto pl-xs-4 pr-xs-4 pt-xs-2 pb-xs-2 fsize-xs-1 f-w-600'} label='ENTRA' />
             </div>
         } */}
-        {artist.flashLeaderboard.status === 'ONGOING' &&
+        {/* {artist.flashLeaderboard.status === 'ONGOING' &&
             <div className={`bg-brand-gradient pl-xs-6 pr-xs-6 pt-xs-4 pb-xs-4 border-radius-06`} onClick={() => navigate(`/artist/${artist.slug}/flash-leaderboard`, { state: artist })}>
                 <p className='fsize-xs-4 f-w-600 black'>CLASSIFICA FLASH ATTIVA</p>
 
                 <Button style='bg-dark lime-400 fsize-xs-3 f-w-600 dark-900 letter-spacing-1' label='ENTRA →' />
             </div>
-        }
+        } */}
         {/* {artist.flashLeaderboard.status === 'PENDING' &&
             <div className='d-flex-row align-items-center j-c-space-between bg-dark-soft border-radius-100 border-lime-1 pl-xs-1 pr-xs-1 pt-xs-1 pb-xs-1 mb-xs-4'>
                 <div className='d-flex-row j-c-center align-items-center pt-xs-1 pb-xs-1 pl-xs-4 pr-xs-4 bg-white-transp15 border-radius-100 w-100'>
@@ -61,9 +61,9 @@ const MessageFlashLeaderboard = ({ artist }) => {
             </div>
         } */}
         {artist.flashLeaderboard.status === 'CLOSED_VISIBLE' &&
-            <div className='d-flex-row align-items-center j-c-space-between bg-dark-soft border-radius-100 border-lime-1 pl-xs-4 pr-xs-2 pt-xs-2 pb-xs-2 mb-xs-4' onClick={() => navigate(`/artist/${artist.slug}/flash-leaderboard`, { state: artist })}>
-                <p className='fsize-xs-2'>CLASSIFICA FLASH TERMINATA</p>
-                <img className='avatar-24 bg-brand-gradient border-radius-100' src={IconRightDark} alt='GO!' />
+            <div className='d-flex-row align-items-center j-c-space-between bg-brand-gradient border-radius-100 pl-xs-4 pr-xs-2 pt-xs-2 pb-xs-2 mb-xs-4' onClick={() => navigate(`/artist/${artist.slug}/flash-leaderboard`, { state: artist })}>
+                <p className='fsize-xs-2 black f-w-600'>CLASSIFICA FLASH TERMINATA</p>
+                <img className='avatar-24 bg-white border-radius-100' src={IconRightDark} alt='GO!' />
             </div>
         }
         </>
