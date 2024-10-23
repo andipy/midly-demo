11446c585ff4c0251e840d5ae05e27b1e50f2f40
tommy <tommaso.matta@studenti.unimi.it>
2024-10-23 15:34:18 +0200

    Fixed profile page structure

--- src/routes/profile.route.jsx
+++ src/routes/profile.route.jsx
@@ -8,7 +8,10 @@ import TextTitle from '../components/text-title.component'
 import Appbar from '../components/appbar.component'
 
 import SpotifyLogo from "../images/icons/icon-spotify-full-green.svg"
-
+import IconRounded from "../images/icons/icon-arrowright.svg"
+import GoldBagde from "../images/illustrations/GOLD.png"
+import SettingsLogo from "../images/icons/icon-settings-white.svg"
+import InfoLogo from "../images/icons/icon-info-white.svg"
 
 function ProfileRoute() {
 
@@ -22,7 +25,7 @@ function ProfileRoute() {
         <TextTitle title={'Profilo'} />
         <div>
         {/* DIV PROFILO FOTO ECC */}
-            <div className='mt-xs-2 d-flex-row align-items-start mb-xs-10'>
+            <div className='mt-xs-2 d-flex-row align-items-start mb-xs-12'>
                 <div className='d-flex-column'>
                     {/* AVATAR */}
                     {currentFan.image ? (
@@ -82,13 +85,79 @@ function ProfileRoute() {
         {/* SOCIAL ACOUNT RICONOSCIMENTI */}
             <section id='social-accounts' className='mt-xs-12'>
                 {/* TITLE */}
+                <h4 className='fsize-xs-5 mb-lg-1 letter-spacing-2 f-w-500 mb-xs-2'>
+                    I tuoi riconoscimenti
+                </h4>
                 {/* LINK */}
+                <a href='/badge'>
+                    <div className='bg-dark-gradient border-radius-1 d-flex-row j-c-space-between align-items-center pt-xs-6 pb-xs-6 pl-xs-6 pr-xs-6'>
+                        <div className='d-flex-row align-items-center j-c-start'>
+                            <img className='w-35 no-shrink social-logo' src={''} alt='badge'></img> {/* MANCA */}
+                            <span className='font-heading fsize-xs-4 f-w-500 letter-spacing-1 no-shrink'>I tuoi badge</span>
+                        </div>
+                        <img className='' src={IconRounded} alt='go'/>
+                    </div> 
+                </a>
             </section>
         </div>
         <div>
             {/* PROFILE SETTINGS */}
+            <div id='profile-settings' className="mt-xs-12 mt-lg-5"> 
+                {/* TITLE */}
+                <h4 className='fsize-xs-5 mb-xs-4 mb-lg-2 letter-spacing-2 f-w-500'>Impostazioni</h4>
+                {/* DIV ITEMS */}
+                <div className='d-flex-column'>
+                    {/* ITEM */}
+                    <a className='d-flex-row j-c-space-between mb-xs-3'>
+                        <div className='d-flex-row align-items-center w-100'>
+                            <img className='mr-xs-2' src={SettingsLogo} alt='SETTINGS'></img>
+                            <h6 className='fsize-xs-3 f-w-300 letter-spacing-1'>Informazioni personali</h6>
+                        </div>
+                        <img className='' src={IconRounded} alt='go'/>
+                    </a>
+                    {/* ITEM */}
+                    <a className='d-flex-row j-c-space-between mb-xs-3'>
+                        <div className='d-flex-row align-items-center w-100'>
+                            <img className='mr-xs-2' src={InfoLogo} alt='SETTINGS'></img>
+                            <h6 className='fsize-xs-3 f-w-300 letter-spacing-1'>FAQs</h6>
+                        </div>
+                        <img className='' src={IconRounded} alt='go'/>
+                    </a>
+                    {/* ITEM */}
+                    <a className='d-flex-row j-c-space-between mb-xs-3'>
+                        <div className='d-flex-row align-items-center w-100'>
+                            <img className='mr-xs-2' src={''} alt='SETTINGS'></img> {/* MANCA */}
+                            <h6 className='fsize-xs-3 f-w-300 letter-spacing-1'>Termini e condizioni fan</h6>
+                        </div>
+                        <img className='' src={IconRounded} alt='go'/>
+                    </a>
+                    {/* ITEM */}
+                    <a className='d-flex-row j-c-space-between mb-xs-3'>
+                        <div className='d-flex-row align-items-center w-100'>
+                            <img className='mr-xs-2' src={''} alt='SETTINGS'></img> {/* MANCA */}
+                            <h6 className='fsize-xs-3 f-w-300 letter-spacing-1'>Privacy e policy fan</h6>
+                        </div>
+                        <img className='' src={IconRounded} alt='go'/>
+                    </a>
+                    {/* ITEM */}
+                    <a className='d-flex-row j-c-space-between mb-xs-3'>
+                        <div className='d-flex-row align-items-center w-100'>
+                            <img className='mr-xs-2' src={''} alt='SETTINGS'></img> {/* MANCA */}
+                            <h6 className='fsize-xs-3 f-w-300 letter-spacing-1'>Cookie policy</h6>
+                        </div>
+                        <img className='' src={IconRounded} alt='go'/>
+                    </a>
+                </div>
+            </div>
             {/* LOGOUT */}
-            {/* MIFLY SRL */}
+            <div className='mt-xs-10 mb-xs-10 mt-lg--4 mb-lg-4'>
+                <h6 className='fsize-xs-3 f-w-500 blue-300 letter-spacing-1'>Esegui il log out</h6>
+            </div>
+            
+            {/* MIDLY SRL */}
+            <div className='t-align-center mt-xs-5 mb-xs-2 mt-lg-2 mb-lg-1'>
+                <span className='grey-300 fsize-xs-1'>© Midly Srl 2024 v3.0</span>
+            </div>
         </div>
         </ContainerDefault>
         <Appbar />
