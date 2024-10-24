a96c4ac807ec1afa4a23132760818d2da2662cdc
tommy <tommaso.matta@studenti.unimi.it>
2024-10-24 10:42:08 +0200

    added more artists in context

--- src/contexts/artists.context.jsx
+++ src/contexts/artists.context.jsx
@@ -7,11 +7,11 @@ export const ArtistsProvider = ({ children }) => {
     const [artists, setArtists] = useState([
         {
             id: 1,
-            slug: 'arctic-monkeys',
-            artistName: 'Arctic Monkeys',
-            image: require('../images/pictures/arcticmonkeys.jpg'),
+            slug: 'lazza',
+            artistName: 'Lazza',
+            image: require('../images/pictures/lazza.jpeg'),
             flashLeaderboard: {
-                status: 'NONE',
+                status: 'PENDING',
             }
         },{
             id: 2,
@@ -31,13 +31,85 @@ export const ArtistsProvider = ({ children }) => {
             }
         },{
             id: 4,
-            slug: 'lazza',
-            artistName: 'Lazza',
-            image: require('../images/pictures/lazza.jpeg'),
+            slug: 'suspect-cb',
+            artistName: 'Suspect CB',
+            image: require('../images/pictures/suspect-cb.jpg'),
             flashLeaderboard: {
-                status: 'PENDING',
+                status: 'ONGOING',
             }
-        }
+        },{
+            id: 5,
+            slug: 'astro',
+            artistName: 'Astro',
+            image: require('../images/pictures/astro.jpg'),
+            flashLeaderboard: {
+                status: 'ONGOING',
+            }
+        },{
+            id: 6,
+            slug: 'alfa',
+            artistName: 'Alfa',
+            image: require('../images/pictures/alfa.jpg'),
+            flashLeaderboard: {
+                status: 'ONGOING',
+            }
+        },{
+            id: 7,
+            slug: 'sick-luke',
+            artistName: 'Sick Luke',
+            image: require('../images/pictures/sick-luke.jpg'),
+            flashLeaderboard: {
+                status: 'ONGOING',
+            }
+        },{
+            id: 8,
+            slug: 'nabi',
+            artistName: 'Nabi',
+            image: require('../images/pictures/nabi.jpg'),
+            flashLeaderboard: {
+                status: 'ONGOING',
+            }
+        },{
+            id: 9,
+            slug: 'mezzosangue',
+            artistName: 'Mezzosangue',
+            image: require('../images/pictures/mezzosangue.jpg'),
+            flashLeaderboard: {
+                status: 'ONGOING',
+            }
+        },{
+            id: 10,
+            slug: 'anna',
+            artistName: 'Anna',
+            image: require('../images/pictures/anna.jpg'),
+            flashLeaderboard: {
+                status: 'ONGOING',
+            }
+        },{
+            id: 11,
+            slug: 'gue',
+            artistName: 'Guè',
+            image: require('../images/pictures/gue.jpg'),
+            flashLeaderboard: {
+                status: 'ONGOING',
+            }
+        },{
+            id: 12,
+            slug: 'marracash',
+            artistName: 'Marracash',
+            image: require('../images/pictures/marracash.jpg'),
+            flashLeaderboard: {
+                status: 'ONGOING',
+            }
+        },{
+            id: 13,
+            slug: 'sfera-ebbasta',
+            artistName: 'Sfera Ebbasta',
+            image: require('../images/pictures/sfera-ebbasta.jpg'),
+            flashLeaderboard: {
+                status: 'ONGOING',
+            }
+        },
     ])
 
     return (
