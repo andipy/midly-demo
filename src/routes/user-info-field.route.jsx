22b90d69ee8633b330d64dfcf2700db27dc54c41
tommy <tommaso.matta@studenti.unimi.it>
2024-11-04 15:36:12 +0100

    Fixed problems

--- src/routes/user-info-field.route.jsx
+++ src/routes/user-info-field.route.jsx
@@ -1,209 +1,77 @@
-import {useContext} from 'react'
+import { useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 
 import { CurrentFanContext } from '../contexts/currentFan.context'
-
 import ContainerDefault from '../layout/container-default.layout'
 import Appbar from '../components/appbar.component'
 import NavbarPersonalInfoField from '../components/navbar-personal-info-field.component'
 
-
 const UserInfoFieldRoute = () => {
-
-    const { currentFan, setCurrentFan } = useContext(CurrentFanContext)
-
+    const { currentFan } = useContext(CurrentFanContext)
     const location = useLocation()
-    const { type } = location.state || {} 
+    const { type } = location.state || {}
 
-  return (
-    <>
-    <NavbarPersonalInfoField title={type} />
-    <ContainerDefault containerSpecificStyle={'pb-xs-appbar'}>
-        <div>
-        {(() => {
-            switch (type) {
-                case 'USERNAME':
-                    return (
-                        <>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    USERNAME
-                                </label>
-                                {currentFan.username ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.username}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Aggiungi il tuo username!</h6>
-                                }
-                            </div>
-                        </>
-                    )
-                case 'INSTAGRAM_USERNAME':
-                    return (
-                        <>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    INSTAGRAM
-                                </label>
-                                {currentFan.instagram ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.instagram}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Aggiungi il tuo instagram!</h6>
-                                }
-                            </div>
-                        </>
-                    )
-                case 'BIRTHDATE':
-                    return (
-                        <>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    DATA DI NASCITA
-                                </label>
-                                {currentFan.birthdate ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.birthdate}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Aggiungi ila tua data di nascita!</h6>
-                                }
-                            </div>
-                        </>
-                    )
-                    
-                case 'GENRE':
-                    return (
-                        <>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    GENERE
-                                </label>
-                                {currentFan.genre ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.genre}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Aggiungi il tuo genere!</h6>
-                                }
-                            </div>
-                        </>
-                    )
-                case 'EMAIL':
-                    return (
-                        <>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    EMAIL
-                                </label>
-                                {currentFan.email ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.email}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Aggiungi la tua mail!</h6>
-                                }
-                            </div>
-                        </>
-                    )
-                case 'CELLPHONE':
-                    return (
-                        <>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    CELLULARE
-                                </label>
-                                {currentFan.cellphone ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.cellphone}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Aggiungi il tuo cellulare!</h6>
-                                }
-                            </div>
-                        </>
-                    )
-                case 'ADDRESS':
-                    return (
-                        <>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    NOME
-                                </label>
-                                {currentFan.adress.name ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.adress.name}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Aggiungi il tuo nome!</h6>
-                                }
-                            </div>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    COGNOME
-                                </label>
-                                {currentFan.adress.surname ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.adress.surname}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Aggiungi il tuo cognome!</h6>
-                                }
-                            </div>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    VIA E NUMERO
-                                </label>
-                                {currentFan.adress.strada ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.adress.strada}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Nesun indirizzo! Aggiungilo</h6>
-                                }
-                            </div>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    CAP
-                                </label>
-                                {currentFan.adress.zipcode ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.adress.zipcode}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Nesun CAP! Aggiungilo</h6>
-                                }
-                            </div>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    COMUNE/CITTA'
-                                </label>
-                                {currentFan.adress.city ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.adress.city}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Nesuna città! Aggiungila</h6>
-                                }
-                            </div>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    PROVINCIA
-                                </label>
-                                {currentFan.adress.province ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.adress.province}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Nesuna provincia! Aggiungila</h6>
-                                }
-                            </div>
-                            <div className='mt-xs-8 mb-xs-8'>
-                                <label className='fsize-xs-1 grey-300 letter-spacing-3'>
-                                    STATO
-                                </label>
-                                {currentFan.adress.state ? 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{currentFan.adress.state}</h6>
-                                : 
-                                    <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>Nesuno stato! Aggiungilo</h6>
-                                }
-                            </div>
-                        </>
-                    )
-                
-                default:
-                    return (
-                        <>
-                            <h4>Modifica Informazioni</h4>
-                            <p>Seleziona un'opzione per modificare le informazioni del tuo account.</p>
-                        </>
-                    )
+    const renderFieldContent = (type) => {
+        switch (type) {
+            case 'USERNAME':
+                return renderField('USERNAME', currentFan.username, 'Aggiungi il tuo username!');
+            case 'INSTAGRAM_USERNAME':
+                return renderField('INSTAGRAM', currentFan.instagram, 'Aggiungi il tuo instagram!');
+            case 'BIRTHDATE':
+                return renderField('DATA DI NASCITA', currentFan.birthdate, 'Aggiungi la tua data di nascita!');
+            case 'GENRE':
+                return renderField('GENERE', currentFan.genre, 'Aggiungi il tuo genere!');
+            case 'EMAIL':
+                return renderField('EMAIL', currentFan.email, 'Aggiungi la tua mail!');
+            case 'CELLPHONE':
+                return renderField('CELLULARE', currentFan.cellphone, 'Aggiungi il tuo cellulare!');
+            case 'ADDRESS':
+                return renderAddressFields(currentFan.adress);
+            default:
+                return (
+                    <>
+                        <h4>Modifica Informazioni</h4>
+                        <p>Seleziona un'opzione per modificare le informazioni del tuo account.</p>
+                    </>
+                )
+        }
+    }
+
+
+    const renderAddressFields = (address) => (
+        <>
+            {renderField('NOME', address.name, 'Aggiungi il tuo nome!')}
+            {renderField('COGNOME', address.surname, 'Aggiungi il tuo cognome!')}
+            {renderField('VIA E NUMERO', address.strada, 'Nessun indirizzo! Aggiungilo')}
+            {renderField('CAP', address.zipcode, 'Nessun CAP! Aggiungilo')}
+            {renderField('COMUNE/CITTA\'', address.city, 'Nessuna città! Aggiungila')}
+            {renderField('PROVINCIA', address.province, 'Nessuna provincia! Aggiungila')}
+            {renderField('STATO', address.state, 'Nessuno stato! Aggiungilo')}
+        </>
+    )
+
+    const renderField = (label, value, placeholder) => (
+        <div className='mt-xs-8 mb-xs-8'>
+            <label className='fsize-xs-1 grey-300 letter-spacing-3'>{label}</label>
+            {value ? 
+                <h6 className='fsize-xs-2 f-w-300 grey-50 letter-spacing-1 mt-xs-2'>{value}</h6>
+            : 
+                <h6 className='fsize-xs-2 f-w-300 grey-400 letter-spacing-1 mt-xs-2'>{placeholder}</h6>
             }
-        })()}   
         </div>
-    </ContainerDefault>
-    <Appbar />
-    </>
+    )
 
-  )
+    return (
+        <>
+            <NavbarPersonalInfoField title={type} />
+            <ContainerDefault containerSpecificStyle={'pb-xs-appbar'}>
+                <div>
+                    {renderFieldContent(type)}
+                </div>
+            </ContainerDefault>
+            <Appbar />
+        </>
+    )
 }
 
 export default UserInfoFieldRoute
