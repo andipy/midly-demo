c40606be10d5df0551fe36b4631e7d0ec3f3ebf1
tommy <tommaso.matta@studenti.unimi.it>
2024-11-15 18:19:07 +0100

    Modified currentFan (da sostituire parametro leaderboardStats)

--- src/contexts/currentFan.context.jsx
+++ src/contexts/currentFan.context.jsx
@@ -37,6 +37,10 @@ export const CurrentFanProvider = ({ children }) => {
         //     {artistId: 2}
         // ],
         fanclubsSubscribed: [],
+        leaderboardStats: {
+            position: 8,
+            points: 111
+        }, /* DA ELIMINARE */
         preferredArtists : [
             {artistId: 1},
             {artistId: 7},
