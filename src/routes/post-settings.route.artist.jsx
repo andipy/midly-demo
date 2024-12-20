e99d075aa73219bc09f5218cbdfb3fa2f816772b
tommy <tommaso.matta@studenti.unimi.it>
2024-12-20 14:44:13 +0100

    Post settings modify

--- src/routes/post-settings.route.artist.jsx
+++ src/routes/post-settings.route.artist.jsx
@@ -11,6 +11,7 @@ import Carousel from '../layout/carousel.layout'
 import Button from '../components/button.component'
 import NavbarCommentsModal from '../components/navbar-comments-modal.component'
 import AudioPost from '../components/audio-post.component'
+import FullPageCenter from '../layout/full-page-center.layout'
 
 import IconCopy from '../images/icons/icon-copy.svg'
 
@@ -19,6 +20,9 @@ const PostSettingsRoute = () => {
     const navigate = useNavigate()
     const { setPostInFocus } = useOutletContext()
     const { state } = useLocation()
+    const {fanclubs, setFanclubs} = useContext(FanclubsContext)
+
+
     const onClick = () => {
         navigate(-1, { state : { ...state, invokedModal: false }})
         setPostInFocus({
@@ -27,6 +31,144 @@ const PostSettingsRoute = () => {
             post: undefined
         })
     }
+
+    const [post, setPost] = useState({})
+    const fetchThisPost = () => {
+        setPost(state)
+    }
+
+    useEffect(() => {
+            if (state) {
+                fetchThisPost()
+            }
+    }, [state])
+
+/*     useEffect(() => {
+        if (post) {
+            console.log(post)
+        }
+    }, [post]) */
+    
+
+    const updatePosts = () => {
+        setFanclubs(prevFanclubs =>
+            prevFanclubs.map(fanclub => {
+                if (fanclub.artistId === post.artistId) {
+                    return {
+                        ...fanclub,
+                        posts: fanclub.posts.map(elem => {
+                            if ( elem.id === post.id ) {
+                                return {
+                                    ...elem,
+                                    mode: 'PUBLISHED',
+                                    caption: post?.caption,
+                                    link: {
+                                        name: post?.link?.name,
+                                        url: post?.link.url
+                                    },
+                                    settings: {
+                                        isPinned: post?.settings?.isPinned,
+                                        isPrivate: post?.settings?.isPrivate
+                                    }
+                                }
+                            }
+                            return elem
+                        })
+                    }
+                }
+                return fanclub
+            })
+        )
+
+        navigate(-1)
+    }
+
+    const handleCaption = (e) => {
+        e.preventDefault()
+        setPost(prev => ({
+            ...prev,
+            caption: e.target.value
+        }))
+    }
+
+    const handleLink = (e) => {
+        e.preventDefault()
+        setPost(prev => ({
+            ...prev,
+            link: {
+                ...prev.link, 
+                url: e.target.value
+            }
+        }))
+    }
+
+    const handleLinkName = (e) => {
+        e.preventDefault()
+        setPost(prev => ({
+            ...prev,
+            link: {
+                ...prev.link, 
+                name: e.target.value
+            }
+        }))
+    }
+
+    const handleIsPinned = (e) => {
+        e.preventDefault()
+        setPost(prev => ({
+            ...prev,
+            settings: {
+                ...prev.settings, 
+                isPinned: !post?.settings?.isPinned
+            }
+        }))
+    }
+    const handleIsPrivate = (e) => {
+        e.preventDefault()
+        setPost(prev => ({
+            ...prev,
+            settings: {
+                ...prev.settings, 
+                isPrivate: !post?.settings?.isPrivate
+            }
+        }))
+    }
+
+    const [modal, setModal] = useState(false)
+
+    const deletePost = () => {
+        setModal(true)
+    }
+
+    const [isExiting, setIsExiting] = useState(false)
+
+    useEffect(() => {
+        if (isExiting) {
+            const endDelay = setTimeout(() => {
+                setModal(false)
+                setIsExiting(false)
+            }, 400)
+
+            return () => clearTimeout(endDelay)
+        }
+    }, [isExiting])
+
+    const deletePostConfirmation = () => {
+        setFanclubs(prevFanclubs =>
+            prevFanclubs.map(fanclub => {
+                if (fanclub.artistId === post.artistId) {
+                    return {
+                        ...fanclub,
+                        posts: fanclub.posts.filter(elem => elem.id !== post.id)
+                    }
+                }
+                
+                return fanclub
+            })
+        )
+        navigate(-1)
+    }
+
 
     return (
         <FullScreenModalLayout background='bg-dark-soft'>
@@ -84,10 +226,10 @@ const PostSettingsRoute = () => {
                         id={`input-caption`}
                         className='bg-dark white fsize-xs-2 f-w-500 white letter-spacing-1 border-radius-02 mt-xs-2'
                         type='text'
-                        placeholder={`${state?.caption ? state?.caption : 'Scrivi una didascalia per il tuo post'}`}
-                        value={state?.caption}
+                        placeholder={`${post?.caption ? post?.caption : 'Scrivi una didascalia per il tuo post'}`}
+                        value={post?.caption}
                         rows={4}
-                        // onChange={(e) => handleCaption(e)} 
+                        onChange={(e) => handleCaption(e)} 
                         style={{ resize: 'none' }}
                     />
                 </div>
@@ -99,9 +241,9 @@ const PostSettingsRoute = () => {
                             id={`input-caption`}
                             className='bg-dark white fsize-xs-2 f-w-500 white letter-spacing-1 border-radius-02 mt-xs-2'
                             type='text'
-                            placeholder={`${state?.link?.url ? state?.link?.url : 'https://...'}`}
-                            value={state?.link?.url}
-                            // onChange={(e) => handleLink(e)} 
+                            placeholder={`${post?.link?.url ? post?.link?.url : 'https://...'}`}
+                            value={post?.link?.url}
+                            onChange={(e) => handleLink(e)} 
                         />
                     </div>
                     <div>
@@ -110,9 +252,9 @@ const PostSettingsRoute = () => {
                             id={`input-caption`}
                             className='bg-dark white fsize-xs-2 f-w-500 white letter-spacing-1 border-radius-02 mt-xs-2'
                             type='text'
-                            placeholder={`${state?.link?.name ? state?.link?.name : 'Dai un nome al link'}`}
-                            value={state?.link?.name}
-                            // onChange={(e) => handleLinkName(e)} 
+                            placeholder={`${post?.link?.name ? post?.link?.name : 'Dai un nome al link'}`}
+                            value={post?.link?.name}
+                            onChange={(e) => handleLinkName(e)} 
                         />
                     </div>
                 </section>
@@ -124,10 +266,10 @@ const PostSettingsRoute = () => {
                     </div>
                     
                     <div
-                        className={`toggle-area ${state?.settings?.isPrivate ? 'toggle-area-on' : 'toggle-area-off'}`}
-                        //onClick={(e) => handleIsPrivate(e)}
+                        className={`toggle-area ${post?.settings?.isPrivate ? 'toggle-area-on' : 'toggle-area-off'}`}
+                        onClick={(e) => handleIsPrivate(e)}
                     >
-                        <div className={`toggle-dot ${state?.settings?.isPrivate ? 'toggle-on' : 'toggle-off'}`}></div>
+                        <div className={`toggle-dot ${post?.settings?.isPrivate ? 'toggle-on' : 'toggle-off'}`}></div>
                     </div>
                 </div>
 
@@ -137,18 +279,44 @@ const PostSettingsRoute = () => {
                         <p className='fsize-xs-1 f-w-300 grey-200'>Potrai spinnarlo in ogni momento</p>
                     </div>
                     <div
-                        className={`toggle-area ${state?.settings?.isPinned ? 'toggle-area-on' : 'toggle-area-off'}`}
-                        //onClick={(e) => handleIsPinned(e)}
+                        className={`toggle-area ${post?.settings?.isPinned ? 'toggle-area-on' : 'toggle-area-off'}`}
+                        onClick={(e) => handleIsPinned(e)}
                     >
-                        <div className={`toggle-dot ${state?.settings?.isPinned ? 'toggle-on' : 'toggle-off'}`}></div>
+                        <div className={`toggle-dot ${post?.settings?.isPinned ? 'toggle-on' : 'toggle-off'}`}></div>
                     </div>
                 </div>
 
                 <Button
+                    style='fsize-xs-3 f-w-600 letter-spacing-1 bg-red-300 black border-radius-04'
+                    onClick={deletePost}
+                    label='Elimina post'
+                />
+
+                <Button
                     style='bg-acid-lime fsize-xs-3 f-w-600 dark-900 letter-spacing-1 mt-xs-4'
+                    onClick={updatePosts}
                     label='Salva'
                 />
             </Container>
+            {modal && 
+                <FullPageCenter style='z-index-1100 bg-black-transp70'>
+                    <Container style={`centered-popup position-absolute d-flex-column align-items-center bg-dark-soft-2 border-radius-04 pt-xs-4 pb-xs-4 pl-xs-4 pr-xs-4 pt-sm-2 pb-sm-2 pl-sm-2 pr-sm-2  ${isExiting ? 'fade-out' : ''} `}>
+                        <div className='d-flex-column align-items-center j-c-center w-100 pt-xs-2 pb-xs-2 pr-xs-2 pl-xs-2'>
+                            <h1 className='fsize-xs-3 f-w-500 t-align-center mb-xs-8'>Stai per eliminare definitivamente questo post, l'azione è irreversibile</h1>
+                            <Button
+                                style='fsize-xs-3 f-w-600 letter-spacing-1 bg-red-300 black border-radius-04'
+                                onClick={deletePostConfirmation}
+                                label='Elimina definitivamente'
+                            />
+                            <Button
+                                style='fsize-xs-3 f-w-400 letter-spacing-1 bg-grey-500 white border-radius-04 mt-xs-4'
+                                onClick={() => setIsExiting(true)}
+                                label='Annulla'
+                            />
+                        </div>
+                    </Container>
+                </FullPageCenter>
+            }
         </FullScreenModalLayout>
     )
 }
