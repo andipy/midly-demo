091b8aedabf53a3957d4d4a883fc03bb9a0c5019
tommy <tommaso.matta@studenti.unimi.it>
2025-01-29 15:14:30 +0100

    Const auraPoints

---
+++ src/utils/aurapoints-functions.js
@@ -0,0 +1,48 @@
+export const SUBSCRIPTION_MONTHLY = 15
+export const SUBSCRIPTION_YEARLY = 180
+
+export const LIKE_POST=1
+export const LIKE_POST_COMMENT=2
+export const LIKE_POST_COMMENT_REPLY=2
+export const CLICK_POST_LINK=3
+export const COMMENT_POST_AS_FIRST=9
+export const COMMENT_POST=5
+export const REPLY_TO_POST_COMMENT=7
+export const SHARE_POST=2 
+export const UNLIKE_POST=-1
+export const UNLIKE_POST_COMMENT=-2
+export const UNLIKE_POST_COMMENT_REPLY=-2
+export const DELETE_POST_COMMENT_AS_FIRST=-9
+export const DELETE_POST_COMMENT=-5
+export const DELETE_POST_COMMENT_REPLY=-7
+export const TRY_REPORT_COMMENT=0
+export const TRY_REPORT_USER=0
+export const REPORT_COMMENT_ACCEPTED=10
+export const REPORT_USER_ACCEPTED=10
+
+export const CREATE_TOPIC=20 
+export const LIKE_TOPIC=1
+export const LIKE_REPLY_TO_TOPIC=2
+export const LIKE_REPLY_TOREPLY_TO_TOPIC=2
+export const REPLY_TO_TOPIC_AS_FIRST=9
+export const REPLY_TO_TOPIC=5
+export const REPLY_TO_REPLY_TO_TOPIC=7
+export const SAVE_TOPIC=3
+export const SHARE_TOPIC=2 
+export const UNLIKE_TOPIC=-1
+export const UNLIKE_REPLY_TO_TOPIC=-2
+export const UNLIKE_REPLY_TOREPLY_TO_TOPIC=-2
+export const UNSAVE_TOPIC=-3
+
+
+export const ATTEND_GROUP_CHAT_EVENT=20 
+export const ATTEND_CONCERT=3
+
+
+/* commenti
+
+SHARE_POST=2 (una sola volta per post)
+ATTEND_GROUP_CHAT_EVENT=20 (assegnati se l'utente ha inviato almeno 10 messaggi, valido una volta per chat)
+CREATE_TOPIC=20 (assegnati solo se almeno 20 diversi utenti rispondono)
+SHARE_TOPIC=2 (una sola volta per post) */
+
