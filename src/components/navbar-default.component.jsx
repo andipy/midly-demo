7138a23a913d25c5cd34d5fbdc803b37f805871e
andipy <bydanielromano@gmail.com>
2025-02-24 09:08:37 +0100

    notifications icon fan app

--- src/components/navbar-default.component.jsx
+++ src/components/navbar-default.component.jsx
@@ -1,11 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+
 import Logo from '../images/logo/logo-text-only-white.svg'
+import IconInbox from '../images/icons/icon-inbox-white.svg'
+import Container from '../layout/container.layout'
 
 const NavbarDefault = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <nav id='top-bar' className='top-bar-area-block d-flex-row align-items-center j-c-center white z-index-999'>
-            <div className='container d-flex-row align-items-center j-c-center'>
-                <img src={Logo} alt='MIDLY' />
-            </div>
+        <nav id='top-bar' className='top-bar-area-block d-flex-row align-items-center j-c-space-between white z-index-999'>
+            <Container style='d-flex-row align-items-center j-c-space-between'>
+                <img className='w-20' src={Logo} alt='MIDLY' />
+
+
+                <div className='avatar-32 d-flex-row align-items-center j-c-center bg-black-transp50 border-radius-100' onClick={() => navigate('activity')}>
+                    <img className='avatar-32' src={IconInbox} alt='M' />
+                </div>
+            </Container>
         </nav>
     )
 }
