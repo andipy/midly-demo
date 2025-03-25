dc3af53be501a15eda9e4a849331a89098fbcb0d
andipy <bydanielromano@gmail.com>
2025-03-25 19:08:42 +0100

    badge

--- src/components/badge-monthly.component.jsx
+++ src/components/badge-monthly.component.jsx
@@ -23,46 +23,50 @@ const BadgeMonthly = ({ badge, transition }) => {
     }
 
     return (
-        <div className='position-relative h-80vh border-radius-08 overflow-all-hidden w-100 bg-black border-dark-soft-1'>
-            {/* Top Section */}
-            <div className='w-100 d-flex-row align-items-center j-c-space-between pl-xs-8 pr-xs-8 pt-xs-2 pb-xs-4 z-index-3 position-relative'>
-                <div className='avatar-28'></div>
-                <span className='font-body f-w-700 letter-spacing-1'>SUPERFAN</span>
-                <img className='avatar-28' src={BadgeLogoIcon} />
-            </div>
+        <div className='w-100 position-relative'>
+            <div className='position-relative h-80vh border-radius-08 overflow-all-hidden w-100 bg-black border-dark-soft-1'>
+                {/* Top Section */}
+                <div className='w-100 d-flex-row align-items-center j-c-space-between pl-xs-8 pr-xs-8 pt-xs-2 pb-xs-4 z-index-3 position-relative'>
+                    <div className='avatar-28'></div>
+                    <span className='font-body f-w-700 letter-spacing-1'>SUPERFAN</span>
+                    <img className='avatar-28' src={BadgeLogoIcon} />
+                </div>
 
-            {/* Username and Position Section */}
-            <div className='d-flex-row j-c-start align-items-center gap-0_5em ml-xs-4 z-index-3 position-relative'>
-                <p className='font-body fsize-xs-4 fsize-lg-10 white f-w-700 line-height-140'>{badge.username}</p>
-                <img className='avatar-24 border-radius-100' src={badge.userImage} />
-            </div>
+                {/* Username and Position Section */}
+                <div className='d-flex-row j-c-start align-items-center gap-0_5em ml-xs-4 z-index-3 position-relative'>
+                    <p className='font-body fsize-xs-4 fsize-lg-10 white f-w-700 line-height-140'>{badge.username}</p>
+                    <img className='avatar-24 border-radius-100' src={badge.userImage} />
+                </div>
 
-            <div className='ml-xs-4 mt-xs-2 z-index-3 position-relative'>
-                <p className='w-70 font-body fsize-xs-9 fsize-lg-10 white f-w-800 line-height-1'>{badge.position}° posto nella classifica mensile di</p>
-            </div>
+                <div className='ml-xs-4 mt-xs-2 z-index-3 position-relative'>
+                    <p className='w-70 font-body fsize-xs-9 fsize-lg-10 white f-w-800 line-height-1'>{badge.position}° posto nella classifica mensile di</p>
+                </div>
 
-            {/* Artist Image Section */}
-            <div className='position-absolute-badge avatar-180 bg-red-300 d-flex-row align-items-center j-c-center border-radius-100 overflow-all-hidden z-index-4'>
-                <img className='w-inherit' src={badge.artistImage} alt="" />
-            </div>
+                {/* Artist Image Section */}
+                <div className='position-absolute-badge avatar-180 bg-red-300 d-flex-row align-items-center j-c-center border-radius-100 overflow-all-hidden z-index-4'>
+                    <img className='w-inherit' src={badge.artistImage} alt="" />
+                </div>
 
-            {/* Bottom Section */}
-            <div className='position-absolute bottom-0 bg-dark-overlay-badge w-100 z-index-3 pt-xs-4 pb-xs-4'>
-                <p className='t-align-center fsize-xs-10 f-w-700 line-height-1'>{badge.artistName}</p>
-                <div className='d-flex-row j-c-space-between align-items-end'>
-                    <div className='d-flex-column ml-xs-4'>
-                        <p className='f-w-700 f-size-xs-4'>{months[badge.month]}</p>
-                        <p className='f-w-700 f-size-xs-4'>{badge.year}</p>
-                    </div>
+                {/* Bottom Section */}
+                <div className='position-absolute bottom-0 bg-dark-overlay-badge w-100 z-index-3 pt-xs-4 pb-xs-4'>
+                    <p className='t-align-center fsize-xs-10 f-w-700 line-height-1'>{badge.artistName}</p>
+                    <div className='d-flex-row j-c-space-between align-items-end'>
+                        <div className='d-flex-column ml-xs-4'>
+                            <p className='f-w-700 f-size-xs-4'>{months[badge.month]}</p>
+                            <p className='f-w-700 f-size-xs-4'>{badge.year}</p>
+                        </div>
 
-                    <div className='avatar-32 d-flex-row align-items-center j-c-center border-radius-04 border-white mr-xs-4'>
-                        <img className='avatar-24' src={IconDownload} />
+                        
                     </div>
                 </div>
+
+                {/* Background Graphic */}
+                <img className='position-absolute-x-y w-inherit opacity-0_9 z-index-1' src={BadgeMonthlyGraphic} />
             </div>
 
-            {/* Background Graphic */}
-            <img className='position-absolute-x-y w-inherit opacity-0_9 z-index-1' src={BadgeMonthlyGraphic} />
+            <div className='avatar-32 d-flex-row align-items-center j-c-center border-radius-04 border-white position-absolute bottom-2 right-2 z-index-5'>
+                <img className='avatar-24' src={IconDownload} />
+            </div>
         </div>
 
         // <div className={`badge d-flex-column border-radius-08 overflow-all-hidden ${transition ? 'badge-fade-transition' : ''}`}>
